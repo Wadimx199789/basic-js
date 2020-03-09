156953927b71d92055fd32aa78435af4b6ceeadb
Wadimx199789 <martinkewichwadim@gmil.com>
2020-03-09 22:11:04 +0300

    feat: add solution for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,12 +1,13 @@
 module.exports = function createDreamTeam(members) {
-  var myarr=[];
- if(!Array.isArray(members))return false;
- else{
-   for(let i=0;i<members.length;i++){
-     if(members[i]===tring){
-
-     }
-   }
- }
-
-};
+  let arr=[];
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  for(let i=0;i<members.length;i++){
+    if(typeof members[i]!=="string")continue;
+    else arr.push(members[i].trim()[0].toUpperCase());
+  }
+  
+  return arr.sort().join('');
+ 
+}
