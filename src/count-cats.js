442baf260927003238c4d4e8d87d624c0cf53b7b
Wadimx199789 <martinkewichwadim@gmail.com>
2020-10-03 12:07:43 +0300

    feat: decided

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,11 +1,18 @@
 module.exports = function countCats(matrix){
-   let count=0;
-  for(let i=0;i<matrix.length;i++){
-     for(let j=0;j<matrix[i].length;j++){
-        if(matrix[i][j]!=="^^") continue;
-       else  count+=1;
-     }
-  }
-  return count;
+   let count=0
+   if(matrix.length==0){
+      return count;
+   }
+   else{
+      for(let i=0;i<matrix.length;i++){
+         for(let j=0;j<matrix[i].length;j++){
+            if(matrix[i][j]==="^^"){
+               count+=1;
+            }
+         }
+      }
+     return count;
+   }
+   
    }
    
